de134825129bce3eaf13298e600a07cf3dd3cf51
maruf2446 <maruf2446@gmail.com>
2023-04-10 20:56:09 +0600

    nav commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import {useLoaderData} from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
+import Jobs from '../Jobs/Jobs';
+import './Home.css'
 
 const Home = () => {
     const jobLists = useLoaderData();
     return (
-        <div>
-            <h2>this is home: {jobLists.length}</h2>
+        <div className='Home-container'>
+            <div className='job-container'>
+                {
+                    jobLists.map(job => <Jobs
+                        key={job.id}
+                        job={job}
+                    ></Jobs>)
+                }
+            </div>
         </div>
     );
 };
