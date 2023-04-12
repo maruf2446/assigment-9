fe82ecafd98586822ce365a1f0e1a6d8faf20273
maruf2446 <maruf2446@gmail.com>
2023-04-12 17:52:29 +0600

    add commit

--- src/Components/BannerPart/BannerPart.jsx
+++ src/Components/BannerPart/BannerPart.jsx
@@ -6,10 +6,10 @@ const BannerPart = () => {
             <div className='col-6'>
                 <h2 className='text-6xl my-4'>Find the Most <span className='text-color'>Exciting Startup</span> Jobs</h2>
                 <p className='my-4'>Discover tens of thousands of employment options with the necessary knowledge. It is the future you. Locate it here. Manage every aspect of your job application process.</p>
-                <button className="btn btn-active btn-color rounded-xl">Lets Started</button>
+                <button className="btn btn-active btn-color rounded-full">Lets Started</button>
             </div>
             <div className='col-6 w-4/5 h-4/5'>
-                <img src='../../assets/banner.png' alt="" />
+                <img src='/assets/banner2.png' alt="" />
             </div>
         </div>
     );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,14 +5,14 @@ import './Header.css'
 const Header = () => {
     return (
          <div className='md:flex justify-between md:sticky top-0 bg-slate-200 p-8'>
-            <h2 className='mr-10 font-extrabold text-3xl'>JOB FINDER</h2>
+            <h2 className='mr-10 font-extrabold text-3xl'>I NEED JOB</h2>
             <nav>
                 <Link className='mr-10 font-semibold text-2xl' to='/'>Home</Link>
                 <Link className='mr-10 font-semibold text-2xl' to='/statistics'>Statistics</Link>
                 <Link className='mr-10 font-semibold text-2xl' to='/appliedJob'>Applied Jobs</Link>
                 <Link className='mr-10 font-semibold text-2xl' to='/blog'>Blog</Link>
             </nav>
-            <button className="btn btn-active btn-color rounded-xl">Star Applying</button>
+            <button className="btn btn-active btn-color rounded-full">Star Applying</button>
          </div>
     );
 };
